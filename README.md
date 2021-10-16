50a5e219b4e4bc562fc78529d312dae99b39e5e1
Lyudmila <milo4kamoya@gmail.com>
2021-10-15 18:14:35 -0700

    README added, post request added

---
+++ README.md
@@ -0,0 +1,1 @@
+## A simple application to subscribe to an automatic newsletter.
--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const request = require("request");
+//const request = require("request");
 const port = 3001;
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   //res.send("Server is running");
-  res.send("HELLO ME");
+  res.sendFile(__dirname + "/signup.html");
+});
+app.post("/", (req, res) => {
+  let firstName = req.body.firstName;
+  let lastName = req.body.lastName;
+  let email = req.body.email;
+
+  console.log(firstName, lastName, email);
 });
 
 app.listen(port, () => {
